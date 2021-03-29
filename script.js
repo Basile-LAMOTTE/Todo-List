8c3550bb83d5ad6062290e401345467ded0c0e77
Basile LAMOTTE <Basile-LAMOTTE>
2021-03-29 17:35:46 +0200

    fixed backspace issues

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-
 //When you press enter, click on the add button
 var input = document.getElementById("myInput");
 
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13)
-  document.getElementById("myBtn").click();
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) document.getElementById("myBtn").click();
 });
 
-//Concrats Message when all task done
+//Congrats Message when all task done
 var parent = document.getElementById("myUL");
 var nodesSameClass = parent.getElementsByClassName("checked");
-var task = document.getElementsByTagName("LI")
+var task = document.getElementsByTagName("LI");
 var nbTask = task.length;
-//var list = document.querySelector('ul');
-document.querySelector('ul').addEventListener('click', function() {
+document.querySelector("ul").addEventListener("click", function () {
   if (nodesSameClass.length + 1 === nbTask) {
     alert("good job!\nYou made it!");
     for (var i = 0; i < task.length; i++) {
@@ -23,10 +20,10 @@ document.querySelector('ul').addEventListener('click', function() {
 });
 
 //Shortcut section
-document.onkeyup = function(e) {
-  var els = document.querySelectorAll('.checked');
+document.onkeyup = function (e) {
+  var els = document.querySelectorAll(".checked");
   var close = document.getElementsByClassName("close");
-  
+
   //UnCheck all when you press space
   if (e.ctrlKey && e.which == 32) {
     for (var i = 0; i < els.length; i++) {
@@ -37,12 +34,10 @@ document.onkeyup = function(e) {
     for (var i = 0; i < close.length; i++) {
       close[i].click();
     }
-  } else if (e.which == 8) {
-    alert(nodesSameClass.length);
   }
 };
 
-// Create a "close" button and append it to each list item
+// create close button
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -57,28 +52,32 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+// "checked" symbol
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
 
-// Create a new list item when clicking on the "Add" button
+// "Add" button
 function newElement() {
   var li = document.createElement("li");
   li.className = "hvr-float hvr-bounce-out";
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("The text fiel is empty");
   } else {
     document.getElementById("myUL").appendChild(li);
@@ -92,11 +91,9 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
-
-// Animation
